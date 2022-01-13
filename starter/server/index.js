ccef1b6b16ebf211e5bb654f6731c4c5f512fac7
emre-ozgun <emre2996ozgun@gmail.com>
2022-01-13 23:57:46 +0300

    bug fix

--- starter/server/index.js
+++ starter/server/index.js
@@ -353,19 +353,3 @@ app.delete('/cart/clear', (req, res) => {
 const PORT = 6969;
 
 app.listen(PORT, () => console.log(`listening on port => ${PORT}`));
-
-// endpoints
-
-// -> get all products (products page)
-
-// -> get single product w/ ID
-
-// -> post to cart (add item to cart)
-
-// -> delete from cart (remove item from cart)
-
-// -> patch cart (toggle amount)
-
-// -> get cart
-
-// -> post checkout
